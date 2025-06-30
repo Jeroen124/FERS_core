50fb50e4491191fdaee7feadad586a64eb7b3f53
Jeroen Hermsen <37536594+Jeroen124@users.noreply.github.com>
2025-06-30 19:39:20 +0200

    Bump version to 0.1.38 and simplify package finding in setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ with open("requirements.txt", "r") as req_file:
 
 setup(
     name="FERS",
-    version="0.1.37",
+    version="0.1.38",
     author="Jeroen Hermsen",
     author_email="info@ferscloud.com",
     description="Finite Element Method library written in Rust with Python interface",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/jeroen124/FERS_core",
-    packages=find_packages(include=["FERS_core", "FERS_core.*"]),
+    packages=find_packages(),
     include_package_data=True,
     package_data={"FERS_core": ["examples/*"]},
     zip_safe=False,
