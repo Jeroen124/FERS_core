7f531178119e52905337de24ce780fc9a779cb21
Jeroen Hermsen <37536594+Jeroen124@users.noreply.github.com>
2025-09-26 15:39:06 +0200

    Add double cantilever example with analysis and visualization support

--- fers_core/examples/181_Visual_Double_cantilever.py
+++ fers_core/examples/181_Visual_Double_cantilever.py
@@ -1,6 +1,7 @@
 import os
 from fers_core import Node, Member, FERS, Material, Section, MemberSet, NodalSupport, NodalLoad
-
+from fers_core.supports.supportcondition import SupportCondition
+import math
 
 # =============================================================================
 # Creating a cantilever with an end_load
@@ -19,19 +20,40 @@ Steel_S235 = Material(name="Steel", e_mod=210e9, g_mod=80.769e9, density=7850, y
 
 # Create a section
 # For example IPE 180: https://eurocodeapplied.com/design/en1993/ipe-hea-heb-hem-design-properties
-section = Section(
-    name="IPE 180 Beam Section", material=Steel_S235, i_z=10.63e-6, i_y=0.819e-6, j=0.027e-6, area=0.00196
+ipe_section = Section.create_ipe_section(
+    name="IPE 180 Beam Section",
+    material=Steel_S235,
+    h=0.177,
+    b=0.091,
+    t_f=0.0065,
+    t_w=0.0043,
+    r=0.009,
 )
 
 # Create member
-beam1 = Member(start_node=node1, end_node=node2, section=section)
-beam2 = Member(start_node=node2, end_node=node3, section=section)
+beam1 = Member(start_node=node1, end_node=node2, section=ipe_section)
+beam2 = Member(start_node=node2, end_node=node3, section=ipe_section, rotation_angle=math.pi / 2)
 
 # Creating a boundary conditions (by default all 6 d.o.f. are constraint)
 wall_support = NodalSupport()
+fix_z_support = NodalSupport(
+    displacement_conditions={
+        "X": SupportCondition.free(),
+        "Y": SupportCondition.free(),
+        "Z": SupportCondition.fixed(),
+    },
+    rotation_conditions={
+        "X": SupportCondition.free(),
+        "Y": SupportCondition.free(),
+        "Z": SupportCondition.free(),
+    },
+)
+
 
 # Assigning the nodal_support to the correct node.
 node1.nodal_support = wall_support
+node2.nodal_support = fix_z_support
+node3.nodal_support = fix_z_support
 
 # =============================================================================
 # Now that the geometrical part is created. Lets create the model and the loadcases
@@ -49,18 +71,31 @@ end_load_case = calculation_1.create_load_case(name="End Load")
 # Apply end load at node2 1 kN downward force (global y-axis) to the loadcase
 nodal_load = NodalLoad(node=node3, load_case=end_load_case, magnitude=-1000, direction=(1, 0, 0))
 
-
-file_path = os.path.join("json_input_solver", "11_double_cantilever.json")
+# Run analysis
+file_path = os.path.join("json_input_solver", "181_Visual_Double_cantilever.json")
 os.makedirs(os.path.dirname(file_path), exist_ok=True)
 calculation_1.save_to_json(file_path, indent=4)
 
+# Step 3: Run FERS calculation
+# ----------------------------
+# Perform the analysis using the saved JSON model file
+print("Running the analysis...")
+calculation_1.run_analysis()
+result_loadcase = calculation_1.results.loadcases["End Load"]
 
-# Run analysis
-# result = FERS_calculation(calculation_1)
+dx_fers_3 = result_loadcase.displacement_nodes["3"].dx
+expected_dx_fers_3 = -0.074714  # From other FEM software
 
-# # Print results
-# print("Displacement at free end (node2):")
-# print(f"dy = {node2.displacement[1]:.6f} m")
+# Compare FERS results with analytical solutions
+print("\nComparison of results:")
+print(f"Deflection at free end (FERS): {dx_fers_3:.6f} m")
+print(f"Deflection at free end (Analytical): {expected_dx_fers_3:.6f} m")
+if abs(dx_fers_3 - expected_dx_fers_3) < 1e-6:
+    print("Deflection matches the analytical solution ✅")
+else:
+    print("Deflection does NOT match the analytical solution ❌")
+
+print()
 
 # print("\nReaction forces at fixed end (node1):")
 # print(f"Fy = {node1.reaction_force[1]:.2f} kN")
