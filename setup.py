ccf829761ed490ff3041605ebcfc345708906f72
Jeroen Hermsen <37536594+Jeroen124@users.noreply.github.com>
2025-10-21 07:48:39 +0200

    Bump version to 0.1.48 and add empty __init__.py files for results and validation modules

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("requirements.txt", "r") as req_file:
 
 setup(
     name="FERS",
-    version="0.1.47",
+    version="0.1.48",
     author="Jeroen Hermsen",
     author_email="info@ferscloud.com",
     description="Finite Element Method library written in Rust with Python interface",
