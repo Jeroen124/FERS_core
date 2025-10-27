3a786abba0a6679ad626cc4fbc8b4d7b9b0964ec
Jeroen Hermsen <37536594+Jeroen124@users.noreply.github.com>
2025-10-27 09:50:32 +0100

    Refactor cantilever examples and update JSON configurations for load cases and combinations

--- fers_core/examples/093_Loadcombinations_hooked_cantilever.py
+++ fers_core/examples/093_Loadcombinations_hooked_cantilever.py
@@ -1,5 +1,5 @@
 import os
-from fers_core import Node, Member, FERS, Material, Section, MemberSet, NodalSupport, NodalLoad
+from fers_core import Node, Member, FERS, Material, Section, MemberSet, NodalSupport, NodalLoad, AnalysisOrder
 from fers_core.supports.supportcondition import SupportCondition
 
 
@@ -14,6 +14,8 @@ calculation_1 = FERS()
 node1 = Node(0, 0, 0)  # Fixed end
 node2 = Node(5, 0, 0)  # Free end
 node3 = Node(5, 5, 0)  # Free end
+node4 = Node(5, 0, -1)  # Rope end
+node5 = Node(5, 0, 1)  # Rope end
 
 # Create material
 Steel_S235 = Material(name="Steel", e_mod=210e9, g_mod=80.769e9, density=7850, yield_stress=235e6)
@@ -28,12 +30,14 @@ section = Section(
 beam1 = Member(start_node=node1, end_node=node2, section=section)
 beam2 = Member(start_node=node2, end_node=node3, section=section)
 
+rope_1 = Member(start_node=node3, end_node=node4, section=section, member_type="TENSION")
+rope_2 = Member(start_node=node3, end_node=node5, section=section, member_type="TENSION")
+
 # Creating a boundary conditions (by default all 6 d.o.f. are constraint)
-wall_support = NodalSupport()
-fix_z_support = NodalSupport(
+simply_supported = NodalSupport(
     displacement_conditions={
-        "X": SupportCondition.free(),
-        "Y": SupportCondition.free(),
+        "X": SupportCondition.fixed(),
+        "Y": SupportCondition.fixed(),
         "Z": SupportCondition.fixed(),
     },
     rotation_conditions={
@@ -43,41 +47,47 @@ fix_z_support = NodalSupport(
     },
 )
 
-
 # Assigning the nodal_support to the correct node.
-node1.nodal_support = wall_support
-node2.nodal_support = fix_z_support
-node3.nodal_support = fix_z_support
+node1.nodal_support = simply_supported
+node2.nodal_support = simply_supported
+node4.nodal_support = simply_supported
+node5.nodal_support = simply_supported
 
 # =============================================================================
 # Now that the geometrical part is created. Lets create the model and the loadcases
 # =============================================================================
 # Create a memberset holding the beam
-membergroup1 = MemberSet(members=[beam1])
-membergroup2 = MemberSet(members=[beam2])
+membergroup = MemberSet(members=[beam1, beam2])
+ropegroup = MemberSet(members=[rope_1, rope_2])
 
 # Adding the memberset to the model
-calculation_1.add_member_set(*[membergroup1, membergroup2])
+calculation_1.add_member_set(*[membergroup, ropegroup])
 
 # Create a loadcase for the end load
-end_load_case = calculation_1.create_load_case(name="End Load")
-dead_load_case = calculation_1.create_load_case(name="Dead Load")
+end_load_case_x = calculation_1.create_load_case(name="End Load x")
+end_load_case_y = calculation_1.create_load_case(name="End Load y")
+end_load_case_z = calculation_1.create_load_case(name="End Load z")
 
 # Apply end load at node2 1 kN downward force (global y-axis) to the loadcase
-nodal_load = NodalLoad(node=node3, load_case=end_load_case, magnitude=-1000, direction=(1, 0, 0))
+nodal_load = NodalLoad(node=node3, load_case=end_load_case_x, magnitude=-100, direction=(1, 0, 0))
+nodal_load = NodalLoad(node=node3, load_case=end_load_case_y, magnitude=-10000, direction=(0, 1, 0))
+nodal_load = NodalLoad(node=node3, load_case=end_load_case_z, magnitude=-100, direction=(0, 0, 1))
 
 ultimate_persistent_combination = calculation_1.create_load_combination(
-    name="ULS persistent",
+    name="A simple combination",
     load_cases_factors={
-        dead_load_case: 1.35,
-        end_load_case: 1.50,
+        end_load_case_x: 1,
+        end_load_case_y: 1,
+        end_load_case_z: 1,
     },
     situation="ULS",
     check="ALL",
 )
 
 # Run analysis
-file_path = os.path.join("json_input_solver", "091_Loadcombinations_cantilever.json")
+calculation_1.settings.analysis_options.order = AnalysisOrder.LINEAR
+
+file_path = os.path.join("json_input_solver", "093_Loadcombinations_hooked_cantilever.json")
 os.makedirs(os.path.dirname(file_path), exist_ok=True)
 calculation_1.save_to_json(file_path, indent=4)
 
@@ -86,25 +96,12 @@ calculation_1.save_to_json(file_path, indent=4)
 # Perform the analysis using the saved JSON model file
 print("Running the analysis...")
 calculation_1.run_analysis()
-result_loadcase = calculation_1.resultsbundle.loadcases["End Load"]
+result_loadcase_1 = calculation_1.resultsbundle.loadcases["End Load x"]
+result_loadcase_2 = calculation_1.resultsbundle.loadcases["End Load y"]
+result_loadcombination = calculation_1.resultsbundle.loadcombinations["A simple combination"]
 
-d_node_2 = result_loadcase.displacement_nodes["2"]
-d_node_3 = result_loadcase.displacement_nodes["3"]
-dy_fers_2 = result_loadcase.displacement_nodes["2"].dy
-dx_fers_3 = result_loadcase.displacement_nodes["3"].dx
+dy_node_3_loadcase_1 = result_loadcase_1.displacement_nodes["3"].dy
+dy_node_3_loadcase_2 = result_loadcase_2.displacement_nodes["3"].dy
 
-# # Print results
-print("Displacement at mid (node2):")
-print(f"dy = {dy_fers_2:.6f} m")
-
-# print("\nReaction forces at fixed end (node1):")
-# print(f"Fy = {node1.reaction_force[1]:.2f} kN")
-# print(f"Mz = {node1.reaction_moment[2]:.2f} kN·m")
-
-# # Calculate and print maximum bending stress
-# i_z = beam.moment_of_inertia_z
-# y_max = beam.height / 2
-# M_max = abs(node1.reaction_moment[2])
-# sigma_max = M_max * y_max / i_z
-
-# print(f"\nMaximum bending stress: {sigma_max:.2f} MPa")
+expected_deformation_loadcases = dy_node_3_loadcase_1 + 2 * dy_node_3_loadcase_2
+dy_node_3_loadcombination = result_loadcombination.displacement_nodes["3"].dy
